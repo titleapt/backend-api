f8ead1a1728cbaa8cda1e29196a046ea870b9239
Apisit  Tanapichartsakul <apisittanapichartsakul@Apisits-MacBook-Air.local>
2023-11-14 15:09:57 +0700

    first commit

---
+++ src/main/kotlin/com/example/beweb/BeWebApplication.kt
@@ -0,0 +1,11 @@
+package com.example.beweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeWebApplication
+
+fun main(args: Array<String>) {
+	runApplication<BeWebApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/beweb/BeWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.beweb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BeWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
